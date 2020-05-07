3748dc109eb110ffedff3cbc0a2190f33f0c0526
wdlairpgv62 <53283430+wdlairpgv62@users.noreply.github.com>
2020-05-07 16:22:32 +0800

    Add files via upload

---
+++ future/assets/amchart/material.js
@@ -0,0 +1,17 @@
+am4internal_webpackJsonp(["8593"], {
+    d66p: function(c, t, e) {
+        "use strict";
+        Object.defineProperty(t, "__esModule", {
+            value: !0
+        });
+        var b = e("aCit"),
+        o = e("8ZqG"),
+        n = function(c) {
+            Object(b.b)(c, "ColorSet") && (c.list = [Object(o.c)("#4ac7ec"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#5d78ff"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#fd397a"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#4CAF50"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#E0E7FD"), Object(o.c)("#FF9800"), Object(o.c)("#FF5722"), Object(o.c)("#795548"), Object(o.c)("#9E9E9E"), Object(o.c)("#607D8B")], c.minLightness = .2, c.maxLightness = .7, c.reuse = !0),
+            Object(b.b)(c, "ResizeButton") && (c.background.cornerRadiusTopLeft = 20, c.background.cornerRadiusTopRight = 20, c.background.cornerRadiusBottomLeft = 20, c.background.cornerRadiusBottomRight = 20),
+            Object(b.b)(c, "Tooltip") && (c.animationDuration = 800)
+        };
+        window.am4themes_material = n
+    }
+},
+["d66p"]);
