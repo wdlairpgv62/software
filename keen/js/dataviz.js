c516c6786f218455815587d2ab863f8834e6974a
wdlairpgv62 <53283430+wdlairpgv62@users.noreply.github.com>
2020-07-25 08:46:19 +0800

    Add files via upload

---
+++ keen/js/dataviz.js
@@ -0,0 +1,19 @@
+am4internal_webpackJsonp(["f6f5"], {
+    JjGr: function(e, t, c) {
+        "use strict";
+        Object.defineProperty(t, "__esModule", {
+            value: !0
+        });
+        var s = c("aCit"),
+        i = c("8ZqG"),
+        n = function(e) {
+            Object(s.b)(e, "ColorSet") && (e.list = [Object(i.c)("#3699ff"),Object(i.c)("#3699ff"), Object(i.c)("#ffb822"), Object(i.c)("#ffb822")], e.reuse = !1, e.stepOptions = {
+                lightness: .05,
+                hue: 0
+            },
+            e.passOptions = {})
+        };
+        window.am4themes_dataviz = n
+    }
+},
+["JjGr"]);
